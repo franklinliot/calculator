1da4067507bf6e87f20896d2eb0de4977b9ea7c2
franklin <franklin.liot@edu.escp.eu>
2021-05-18 15:03:21 +0200

    fini la fonction d'ajout des nombres à l'array

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
+num1 = ""
+array = []
+
+function zero() {
+    num1 += 0
+}
+
+function one() {
+    num1 += 1
+}
+
+function two() {
+    num1 += 2
+}
+
+function three() {
+    num1 += 3
+}
+
+function four() {
+    num1 += 4
+}
+
+function five() {
+    num1 += 5
+}
+
+function six() {
+    num1 += 6
+}
+
+function seven() {
+    num1 += 7
+}
+
+function eight() {
+    num1 += 8
+}
+
+function nine() {
+    num1 += 9
+}
+
+function clearNum1() {
+    num1 = Number(num1)
+    array.push(num1)
+    num1 = "";
+}
+
+function add() {
+    clearNum1()
+}
+
+function subtract() {
+    clearNum1()
+}
+
+function multiply() {
+    clearNum1()
+}
+
+
+
+
+
+
+
+
+
+
 /*
   function askNum() {
       x = Number(prompt())
@@ -20,56 +90,3 @@
 
   }
 */
-
-num1 = ""
-
-  function zero() {
-      num1 += 0
-
-  }
-
-  function one() {
-      num1 += 1
-
-  }
-
-  function two() {
-      num1 += 2
-
-  }
-
-  function three() {
-      num1 += 3
-
-  }
-
-  function four() {
-      num1 += 4
-
-  }
-
-  function five() {
-      num1 += 2
-
-  }
-
-  function six() {
-      num1 += 6
-
-  }
-
-  function seven() {
-      num1 += 7
-
-  }
-
-  function eight() {
-      num1 += 8
-
-  }
-
-  function nine() {
-      num1 += 9
-
-  }
-
