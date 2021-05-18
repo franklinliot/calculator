252d13924ba53ddc44a1c63cb19654c61b7af95e
franklin <franklin.liot@edu.escp.eu>
2021-05-18 14:46:27 +0200

    boutons créés, possibilité d'ajouter les digits à un seul nombre

---
+++ script.js
@@ -0,0 +1,75 @@
+/*
+  function askNum() {
+      x = Number(prompt())
+      y = Number(prompt())
+  }
+
+  function calculator() {
+      askNum()
+      let type = prompt()
+      if (type === "add") {
+          result = x + y;
+          console.log(result);
+      } else if (type === "subtract") {
+          result = x - y;
+          console.log(result);
+      } else {
+          result = x * y;
+          console.log(result);
+      }
+
+  }
+*/
+
+num1 = ""
+
+  function zero() {
+      num1 += 0
+
+  }
+
+  function one() {
+      num1 += 1
+
+  }
+
+  function two() {
+      num1 += 2
+
+  }
+
+  function three() {
+      num1 += 3
+
+  }
+
+  function four() {
+      num1 += 4
+
+  }
+
+  function five() {
+      num1 += 2
+
+  }
+
+  function six() {
+      num1 += 6
+
+  }
+
+  function seven() {
+      num1 += 7
+
+  }
+
+  function eight() {
+      num1 += 8
+
+  }
+
+  function nine() {
+      num1 += 9
+
+  }
+
